82abe16ab9662b7c65acb51a88cad65bd2b1edad
Saurabh Uthaman <saurabh.uthaman@IN-MUM-PR-WK112.GLOBALSERVS>
2025-06-15 22:55:38 +0530

    added changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images:{
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:"saurabhuthaman.github.io"
+      },
+    ]
+  },
   /* config options here */
 };
 
